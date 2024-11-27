8b8ebfcf769153730b1f8843cc7c942527d5f11a
Shawn TIAN <hightian@gmail.com>
2024-11-27 14:56:32 +0800

    add attestation code and docs

--- hub/examples/confidential_chat_openai_client.py
+++ hub/examples/confidential_chat_openai_client.py
@@ -2,6 +2,7 @@ import openai
 import json
 import os
 import nearai
+import requests
 
 hub_url = "https://inference-api.phala.network/v1"
 
@@ -13,9 +14,19 @@ signature = json.dumps(auth)
 client = openai.OpenAI(base_url=hub_url, api_key=signature)
 
 
-# create a chat completion
+# Create a chat completion
 chat_completion = client.chat.completions.create(
     model="meta-llama/meta-llama-3.1-8b-instruct",
     messages=[{"role": "user", "content": "Hello, world!"}],
 )
 print(chat_completion)
+
+
+# Get Attestation & Public Key
+response = requests.get(f"{hub_url}/attestation/report")
+attestation_report = response.json()
+print("Attestation report:", attestation_report)
+
+# Verify the Attestation
+# The verify procedures can be found in following link:
+# https://docs.phala.network/confidential-ai-inference/confidential-ai-api#verify-the-attestation
