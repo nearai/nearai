ebfade7fb966dce23aa65194f7df9fcf1d3781e0
Shawn TIAN <hightian@gmail.com>
2024-11-27 14:46:58 +0800

    add nearai login and credentials

--- hub/examples/confidential_chat_openai_client.py
+++ hub/examples/confidential_chat_openai_client.py
@@ -1,9 +1,17 @@
 import openai
+import json
+import os
+import nearai
 
 hub_url = "https://inference-api.phala.network/v1"
 
+# Login to NEAR AI Hub using nearai CLI.
+# Read the auth object from ~/.nearai/config.json
+auth = nearai.config.load_config_file()["auth"]
+signature = json.dumps(auth)
 
-client = openai.OpenAI(base_url=hub_url, api_key="-")
+client = openai.OpenAI(base_url=hub_url, api_key=signature)
+
 
 # create a chat completion
 chat_completion = client.chat.completions.create(
