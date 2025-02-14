7b9da161983bbbf5dae4dd12d6b2e13318b6dde2
Gabe Hamilton <gabe@near.ai>
2025-02-14 14:03:15 -0700

    Runner setting for whether to use agent cache (false on staging)

--- nearai/aws_runner/service.py
+++ nearai/aws_runner/service.py
@@ -133,16 +133,19 @@ def load_agent(client, agent, params: dict, additional_path: str = "", verbose=T
     agent_metadata = None
 
     if params["data_source"] == "registry":
-        global local_agent_cache
-        if agent in local_agent_cache:
-            print(f"Using {agent} from cache")
-            full_agent = local_agent_cache[agent]
+        use_cache = os.getenv("USE_AGENT_CACHE", "true").lower() == "true"
 
-            if full_agent.agent_language == "ts":
-                full_agent.temp_dir = full_agent.write_agent_files_to_temp(full_agent.agent_files)
-                return full_agent
-            else:
-                return local_agent_cache[agent]
+        if use_cache:
+            global local_agent_cache
+            if agent in local_agent_cache:
+                print(f"Using {agent} from cache")
+                full_agent = local_agent_cache[agent]
+
+                if full_agent.agent_language == "ts":
+                    full_agent.temp_dir = full_agent.write_agent_files_to_temp(full_agent.agent_files)
+                    return full_agent
+                else:
+                    return local_agent_cache[agent]
 
         start_time = time.perf_counter()
         agent_files = client.get_agent(agent)
