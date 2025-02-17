63c2e111e4e50be4e61dbb98781d336bdbcb793b
Andre Gustavo <andre.gustavo@joinplank.com>
2025-02-17 16:01:47 -0300

    Clean-up

--- infra/terraform/modules/datadog-log-forwarder/main.tf
+++ infra/terraform/modules/datadog-log-forwarder/main.tf
@@ -12,7 +12,6 @@ resource "aws_secretsmanager_secret_version" "dd_api_key" {
 #   }
 }
 
-# Use the Datadog Forwarder to ship logs from S3 and CloudWatch, as well as observability data from Lambda functions to Datadog. For more information, see https://github.com/DataDog/datadog-serverless-functions/tree/master/aws/logs_monitoring
 resource "aws_cloudformation_stack" "datadog_forwarder" {
   name         = "datadog-forwarder"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
