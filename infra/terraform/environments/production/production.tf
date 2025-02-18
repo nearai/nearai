4e0a6d0e6e3cfe1ffa16385d24557b8ccb14769f
andregustavo-plank <andre.gustavo@joinplank.com>
2025-02-18 14:01:28 -0300

    Adding other cloud watch log groups

--- infra/terraform/environments/production/production.tf
+++ infra/terraform/environments/production/production.tf
@@ -11,9 +11,12 @@ module "datadog-log-forwarder" {
   environment = local.environment
   aws_region = local.aws_region
   cloudwatch_log_groups = {
-    "production-agent-runner-agentkit"    = "/aws/lambda/production-agent-runner-agentkit"
     "production-agent-runner-base" = "/aws/lambda/production-agent-runner-base"
+    "production-agent-runner-agentkit"    = "/aws/lambda/production-agent-runner-agentkit"
     "production-agent-runner-web-agent" = "/aws/lambda/production-agent-runner-web-agent"
+    "production-agent-runner-langgraph-0-1-4" = "/aws/lambda/production-agent-runner-langgraph-0-1-4"
+    "production-agent-runner-langgraph-0-2-26" = "/aws/lambda/production-agent-runner-langgraph-0-2-26"
+    "production-agent-runner-masa" = "/aws/lambda/production-agent-runner-masa"
+    "production-agent-runner-ts" = "/aws/lambda/production-agent-runner-ts"
   }
 }
-
