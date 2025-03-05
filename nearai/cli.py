51198b2b0126477d00dac6c3b15b6d64f443fc53
Josh Ford <thisisjoshford@gmail.com>
2025-03-05 10:42:58 -0800

    fix: use exit over return

--- nearai/cli.py
+++ nearai/cli.py
@@ -201,7 +201,7 @@ class RegistryCli:
         """
         if CONFIG.auth is None:
             print("Please login with `nearai login`")
-            return
+            exit(1)
 
         path = resolve_local_path(Path(local_path))
         metadata_path = path / "metadata.json"
