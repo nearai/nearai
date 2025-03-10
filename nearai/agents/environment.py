60c3fc4bc3fcd003a2281c80abacd6e928569247
Mikkel Garcia <mikkel@255bits.com>
2025-03-10 11:47:11 -0600

    wip stream debugging

--- nearai/agents/environment.py
+++ nearai/agents/environment.py
@@ -1310,7 +1310,7 @@ class Environment(object):
 
     def add_reply_streaming(self, results):
         for result in results:
-            print("Streaming:", result.choices[0].delta.content)
+            print("Streaming:", result.choices[0].delta.content, end='', flush=True)
         print("Got here", results, type(results))
         return
         #TODO should this be add_reply?
