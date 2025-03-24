63fc990c4f9df79b09867fd45648cc2be57af6e5
Mikkel Garcia <mikkel@255bits.com>
2025-03-24 15:18:14 -0600

    dont invoke DB unless theres a run_id

--- hub/api/v1/completions.py
+++ hub/api/v1/completions.py
@@ -19,18 +19,17 @@ class Provider(Enum):
     FIREWORKS = "fireworks"
     LOCAL = "local"
 
-
 async def handle_stream(db, thread_id, run_id, message_id, resp_stream, add_usage_callback: Callable):
     response_chunks = []
 
-    for chunk in resp_stream:
-        c = json.dumps(chunk.model_dump())
-        response_chunks.append(c)
-
+    if run_id is not None:
         with get_session() as session:
-            txt = chunk.choices[0].delta.content
-            if txt is not None:
-                if run_id is not None:
+            for chunk in resp_stream:
+                c = json.dumps(chunk.model_dump())
+                response_chunks.append(c)
+
+                txt = chunk.choices[0].delta.content
+                if txt is not None:
                     content = {"content":[{"index":0,"type":"text","text":{"value":txt}}]}
                     delta = Delta(
                         event="thread.message.delta",
@@ -43,27 +42,28 @@ async def handle_stream(db, thread_id, run_id, message_id, resp_stream, add_usag
                     session.add(delta)
                     session.commit()
                     print("Writing Delta obect",run_id,thread_id, message_id)
+                yield f"data: {c}\n\n"
+                await asyncio.sleep(0) # lets the event loop yield, otherwise it batches yields
+        delta = Delta(
+            event="thread.run.completed",
+            created_at=datetime.now(),
+            run_id=run_id,
+            thread_id=thread_id,
+            message_id=message_id
+        )
+        session.add(delta)
+        session.commit()
 
-        yield f"data: {c}\n\n"
-        await asyncio.sleep(0) # lets the event loop yield, otherwise it batches yields
-
-    # TODO: do this at the end of the agent run
-    delta = Delta(
-        event="thread.run.completed",
-        created_at=datetime.now(),
-        run_id=run_id,
-        thread_id=thread_id,
-        message_id=message_id
-    )
-    session.add(delta)
-    session.commit()
-    ##
+    else:
+        for chunk in resp_stream:
+            c = json.dumps(chunk.model_dump())
+            response_chunks.append(c)
+            yield f"data: {c}\n\n"
+            await asyncio.sleep(0) # lets the event loop yield, otherwise it batches yields
 
     yield "data: [DONE]\n\n"
     full_response_text = "".join(response_chunks)
-
     add_usage_callback(full_response_text)
-
 
 def get_llm_ai(provider: str) -> OpenAI:
     if provider == "hyperbolic":
