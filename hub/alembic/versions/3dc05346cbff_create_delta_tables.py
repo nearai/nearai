6b5f29a434a6c864fcd126c6a7dd189d0bf56de1
Gabe Hamilton <gabe@near.ai>
2025-03-31 16:36:05 -0600

    clean up deltas

--- hub/alembic/versions/3dc05346cbff_create_delta_tables.py
+++ hub/alembic/versions/3dc05346cbff_create_delta_tables.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "3dc05346cbff"
-down_revision: Union[str, None] = "212e5ca48274"
+down_revision: Union[str, None] = "fc7a0b228355"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -19,15 +19,25 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "deltas",
-        sa.Column("id", sa.String(length=50), primary_key=True),
+        sa.Column("id", sa.Integer(), primary_key=True, autoincrement=True),
         sa.Column("object", sa.String(length=50), nullable=False),
         sa.Column("created_at", sa.DateTime, server_default=sa.func.now()),
         sa.Column("content", sa.JSON, nullable=True),
         sa.Column("step_details", sa.JSON, nullable=True),
-        sa.Column("metadata", sa.JSON, nullable=True),
         sa.Column("filename", sa.String(length=4096), nullable=True),
+        sa.Column("run_id", sa.String(length=255), nullable=True),
+        sa.Column("thread_id", sa.String(length=255), nullable=True),
+        sa.Column("message_id", sa.String(length=255), nullable=True),
     )
+
+    op.create_index("ix_deltas_run_id", "deltas", ["run_id"])
+    op.create_index("ix_deltas_thread_id", "deltas", ["thread_id"])
+    op.create_index("ix_deltas_message_id", "deltas", ["message_id"])
 
 
 def downgrade() -> None:
+    op.drop_index("ix_deltas_run_id", table_name="deltas")
+    op.drop_index("ix_deltas_thread_id", table_name="deltas")
+    op.drop_index("ix_deltas_message_id", table_name="deltas")
+
     op.drop_table("deltas")
